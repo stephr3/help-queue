b203a2f02e1c267be7358d561d0ab1f5d3fb8052
Stephanie Gurung <stephr3@gmail.com>
2016-08-22 20:55:32 -0700

    delete an issue when user has resolved the issue

--- app/routes/issue.js
+++ app/routes/issue.js
@@ -1,4 +1,10 @@
 import Ember from 'ember';
 
 export default Ember.Route.extend({
+  actions: {
+    delete(model) {
+      model.destroyRecord();
+      this.transitionTo('index');
+    }
+  }
 });
